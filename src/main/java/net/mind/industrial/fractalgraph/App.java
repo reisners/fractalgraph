fa580d859283ac5618b492135801e5a43b6b1b70
Dr. Stefan Reisner <stefan.reisner@springer.com>
2019-05-28 00:00:18 +0200

    updated App.java
    refactoring to add circular nodes

--- src/main/java/net/mind/industrial/fractalgraph/App.java
+++ src/main/java/net/mind/industrial/fractalgraph/App.java
@@ -1,11 +1,13 @@
 package net.mind.industrial.fractalgraph;
 
+import lombok.Data;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.commons.math3.complex.Complex;
 
 import java.awt.*;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.function.BiConsumer;
 
 /**
  * Hello world!
@@ -46,98 +48,144 @@ public class App
     private void run() throws IOException {
         double rotate1 = 0;
 
-        double scaleProgression = Math.pow(0.25, 0.2); // scale down to 1/4th in fve steps
+        double scaleProgression = Math.pow(0.25, 1.0/8); // scale down to 1/4th in 8 steps
 
         // first image
         double scale1 = scale;
         Complex[] world = geometryGenerator.generate(5);
-        Point2D[] view1 = transform(world, scale1, rotate1);
-        svgGenerator.generateSVG(g2d -> {
-            drawLine(g2d, view1[1], view1[2]);
-            drawLine(g2d, view1[2], view1[3]);
-            drawLine(g2d, view1[3], view1[1]);
-            drawLine(g2d, view1[4], view1[5]);
-            drawLine(g2d, view1[5], view1[6]);
-            drawLine(g2d, view1[6], view1[4]);
-        }, new FileWriter("img/img1.svg"));
+        int index = 0;
 
-        double scale2 = scale1 * scaleProgression;
-        Point2D[] view2 = transform(world, scale2, rotate1);
-        svgGenerator.generateSVG(g2d -> {
-            drawLine(g2d, view2[1], view2[2]);
-            drawLine(g2d, view2[2], view2[3]);
-            drawLine(g2d, view2[3], view2[1]);
-            drawLine(g2d, view2[4], view2[5]);
-            drawLine(g2d, view2[5], view2[6]);
-            drawLine(g2d, view2[6], view2[4]);
-            drawLine(g2d, view2[4], view2[7]);
-            drawLine(g2d, view2[5], view2[8]);
-            drawLine(g2d, view2[6], view2[9]);
-        }, new FileWriter("img/img2.svg"));
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+        });
 
-        double scale3 = scale2 * scaleProgression;
-        Point2D[] view3 = transform(world, scale3, rotate1);
-        svgGenerator.generateSVG(g2d -> {
-            drawLine(g2d, view3[1], view3[2]);
-            drawLine(g2d, view3[2], view3[3]);
-            drawLine(g2d, view3[3], view3[1]);
-            drawLine(g2d, view3[4], view3[5]);
-            drawLine(g2d, view3[5], view3[6]);
-            drawLine(g2d, view3[6], view3[4]);
-            drawLine(g2d, view3[4], view3[7]);
-            drawLine(g2d, view3[5], view3[8]);
-            drawLine(g2d, view3[6], view3[9]);
-            drawLine(g2d, view3[7], view3[11]);
-            drawLine(g2d, view3[8], view3[12]);
-            drawLine(g2d, view3[9], view3[10]);
-        }, new FileWriter("img/img3.svg"));
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 0.5f);
+        });
 
-        double scale4 = scale3 * scaleProgression;
-        Point2D[] view4 = transform(world, scale4, rotate1);
-        svgGenerator.generateSVG(g2d -> {
-            drawLine(g2d, view4[1], view4[2]);
-            drawLine(g2d, view4[2], view4[3]);
-            drawLine(g2d, view4[3], view4[1]);
-            drawLine(g2d, view4[4], view4[5]);
-            drawLine(g2d, view4[5], view4[6]);
-            drawLine(g2d, view4[6], view4[4]);
-            drawLine(g2d, view4[4], view4[7]);
-            drawLine(g2d, view4[5], view4[8]);
-            drawLine(g2d, view4[6], view4[9]);
-            drawLine(g2d, view4[7], view4[11]);
-            drawLine(g2d, view4[8], view4[12]);
-            drawLine(g2d, view4[9], view4[10]);
-            drawLine(g2d, view4[10], view4[7]);
-            drawLine(g2d, view4[11], view4[8]);
-            drawLine(g2d, view4[12], view4[9]);
-        }, new FileWriter("img/img4.svg"));
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+        });
 
-        double scale5 = scale4 * scaleProgression;
-        Point2D[] view5 = transform(world, scale5, rotate1);
-        svgGenerator.generateSVG(g2d -> {
-            drawLine(g2d, view5[1], view5[2]);
-            drawLine(g2d, view5[2], view5[3]);
-            drawLine(g2d, view5[3], view5[1]);
-            drawLine(g2d, view5[4], view5[5]);
-            drawLine(g2d, view5[5], view5[6]);
-            drawLine(g2d, view5[6], view5[4]);
-            drawLine(g2d, view5[4], view5[7]);
-            drawLine(g2d, view5[5], view5[8]);
-            drawLine(g2d, view5[6], view5[9]);
-            drawLine(g2d, view5[7], view5[11]);
-            drawLine(g2d, view5[8], view5[12]);
-            drawLine(g2d, view5[9], view5[10]);
-            drawLine(g2d, view5[10], view5[7]);
-            drawLine(g2d, view5[11], view5[8]);
-            drawLine(g2d, view5[12], view5[9]);
-            drawLine(g2d, view5[7], view5[6]);
-            drawLine(g2d, view5[8], view5[4]);
-            drawLine(g2d, view5[9], view5[5]);
-        }, new FileWriter("img/img5.svg"));
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+            drawGeo2(view, g2d, 0.5f);
+        });
+
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+            drawGeo2(view, g2d, 1);
+        });
+
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+            drawGeo2(view, g2d, 1);
+            drawGeo3(view, g2d, 0.5f);
+        });
+
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+            drawGeo2(view, g2d, 1);
+            drawGeo3(view, g2d, 1);
+        });
+
+        renderSVG(rotate1, scaleProgression, scale1, world, index++, (g2d, view) -> {
+            drawGeo1(view, g2d);
+            drawGeo1a(view, g2d, 1);
+            drawGeo2(view, g2d, 1);
+            drawGeo3(view, g2d, 1);
+            drawGeo4(view, g2d, 0.5f);
+        });
+
     }
 
-    private void drawLine(SVGGraphics2D g2d, Point2D pstart, Point2D pend) {
-        g2d.drawLine(pstart.getX(), pstart.getY(), pend.getX(), pend.getY());
+    @Data
+    private static class Geometry {
+        private final Point2D[] view;
+        private final int index;
+        private final double scaleProgression;
+    }
+
+    private void renderSVG(double rotate1, double scaleProgression, double scale1, Complex[] world, int index, BiConsumer<SVGGraphics2D, Geometry> biConsumer) throws IOException {
+        svgGenerator.generateSVG(g2d -> {
+            double scale = scale1 * Math.pow(scaleProgression, index);
+            biConsumer.accept(g2d, new Geometry(transform(world, scale, rotate1), index, scaleProgression));
+        }, new FileWriter("img/img"+index+".svg"));
+    }
+
+    private void drawGeo4(Geometry geometry, SVGGraphics2D g2d, float phase) {
+        Point2D[] view = geometry.getView();
+        drawLine(g2d, view[7], view[6], phase);
+        drawLine(g2d, view[8], view[4], phase);
+        drawLine(g2d, view[9], view[5], phase);
+    }
+
+    private void drawGeo3(Geometry geometry, SVGGraphics2D g2d, float phase) {
+        Point2D[] view = geometry.getView();
+        drawLine(g2d, view[10], view[7], phase);
+        drawLine(g2d, view[11], view[8], phase);
+        drawLine(g2d, view[12], view[9], phase);
+    }
+
+    private void drawGeo2(Geometry geometry, SVGGraphics2D g2d, float phase) {
+        Point2D[] view = geometry.getView();
+        drawLine(g2d, view[7], view[11], phase);
+        drawLine(g2d, view[8], view[12], phase);
+        drawLine(g2d, view[9], view[10], phase);
+    }
+
+    private void drawGeo1a(Geometry geometry, SVGGraphics2D g2d, float phase) {
+        Point2D[] view = geometry.getView();
+        drawLine(g2d, view[4], view[7], phase);
+        drawLine(g2d, view[5], view[8], phase);
+        drawLine(g2d, view[6], view[9], phase);
+    }
+
+    private void drawGeo1(Geometry geometry, SVGGraphics2D g2d) {
+        Point2D[] view = geometry.getView();
+        double scale0 = 10 * Math.pow(geometry.getScaleProgression(), geometry.getIndex());
+        double scale1 = 10 * Math.pow(geometry.getScaleProgression(), geometry.getIndex() + 1);
+        double scale2 = 10 * Math.pow(geometry.getScaleProgression(), geometry.getIndex() + 2);
+        double scale3 = 10 * Math.pow(geometry.getScaleProgression(), geometry.getIndex() + 3);
+
+        drawCircle(g2d, view[4], scale3);
+        drawCircle(g2d, view[5], scale3);
+        drawCircle(g2d, view[6], scale3);
+        drawCircle(g2d, view[7], scale2);
+        drawCircle(g2d, view[8], scale2);
+        drawCircle(g2d, view[9], scale2);
+        drawCircle(g2d, view[10], scale1);
+        drawCircle(g2d, view[11], scale1);
+        drawCircle(g2d, view[12], scale1);
+        drawCircle(g2d, view[13], scale0);
+        drawCircle(g2d, view[14], scale0);
+        drawCircle(g2d, view[15], scale0);
+        drawLine(g2d, view[1], view[2], 1);
+        drawLine(g2d, view[2], view[3], 1);
+        drawLine(g2d, view[3], view[1], 1);
+        drawLine(g2d, view[4], view[5], 1);
+        drawLine(g2d, view[5], view[6], 1);
+        drawLine(g2d, view[6], view[4], 1);
+    }
+
+    private void drawCircle(SVGGraphics2D g2d, Point2D pcenter, double radius) {
+        int iradius = (int) Math.round(radius);
+        g2d.fillOval(pcenter.getX()-iradius, pcenter.getY()-iradius, 2*iradius, 2*iradius);
+    }
+
+    private void drawLine(SVGGraphics2D g2d, Point2D pstart, Point2D pend, float phase) {
+        if (phase == 1) {
+            g2d.drawLine(pstart.getX(), pstart.getY(), pend.getX(), pend.getY());
+        } else {
+            g2d.drawLine(pstart.getX(), pstart.getY(), Math.round(pstart.getX() + (pend.getX() - pstart.getX())*phase/2), Math.round(pstart.getY() + (pend.getY() - pstart.getY())*phase/2));
+            g2d.drawLine(pend.getX(), pend.getY(), Math.round(pend.getX() + (pstart.getX() - pend.getX())*phase/2), Math.round(pend.getY() + (pstart.getY() - pend.getY())*phase/2));
+        }
     }
 
     private Point2D[] transform(Complex[] worldPoints, double scale, double rotate) {
